dda99b22e993c40d4aa86b39ef073ed3a235135a
Ravindranath Mohanraj <ravindranath.mohanraj@astrazeneca.com>
2019-06-11 09:02:10 +0530

    Updates

---
+++ precache-manifest.c5fa8918f4d52a986c275bb59349568e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a51f3ba746707ec6628c6cfd557979",
+    "url": "/trelloBoards/index.html"
+  },
+  {
+    "revision": "0af3ff431554df07fb8f",
+    "url": "/trelloBoards/static/css/main.ae768b8c.chunk.css"
+  },
+  {
+    "revision": "25e0a5f9208c4943c5d9",
+    "url": "/trelloBoards/static/js/2.1a74b49b.chunk.js"
+  },
+  {
+    "revision": "0af3ff431554df07fb8f",
+    "url": "/trelloBoards/static/js/main.7a684885.chunk.js"
+  },
+  {
+    "revision": "e678f9c9a6b8c18bd945",
+    "url": "/trelloBoards/static/js/runtime~main.48846dc2.js"
+  }
+]);
